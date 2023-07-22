c263eca66e34d4d0d1ecc55f59eccf2cc6f2c704
Vo Van Truong <vvtruong1007@gmail.com>
2023-07-22 23:09:15 +0700

    commit

---
+++ src/utils/services.ts
@@ -0,0 +1,36 @@
+import { comicsProps, topComicsProps } from "@/types/typeProps";
+import sendRequest from "@/utils/sendRequest";
+
+// ======================== GET ALL GENRES COMICS ================ //
+export const getGenresComic = async () => {
+    try {
+        const { data } = await sendRequest.get("/genres");
+        return data
+    } catch (error) {
+        console.log('Error');
+    }
+}
+
+// ===================== GET TRENDING ============================ //
+export const getTrending = async ({ page, limit }: topComicsProps) => {
+    try {
+        const { data } = await sendRequest.get(`/trending-comics${page ? `?page=${page}` : ''}`) // Call API lấy top truyện trending
+        return limit ? data.comics.filter((item: comicsProps, index: number) => index < limit) : data.comics;
+
+    } catch (error) {
+        console.log('Error');
+    }
+}
+
+
+// ===================== GET TOP COMICS ============================ //
+export const getTopComics = async ({ type, limit, page, status }: topComicsProps) => {
+    try {
+        const { data } = await sendRequest.get(`/top${type ? (type === 'all' ? '' : `/${type}`) : ''} ${page ? `?page=${page}` : ''} ${status ? `&status=${status}` : ''}`) // Call API lấy top truyện trending
+
+        return limit ? data.comics.filter((item: comicsProps, index: number) => index < limit) : data.comics;
+
+    } catch (error) {
+        console.log('Error');
+    }
+}
