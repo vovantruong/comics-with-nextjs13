16237d880022ec92c5e6b4adc15072b1b12d141c
Vo Van Truong <vvtruong1007@gmail.com>
2023-09-20 15:19:02 +0700

    commit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,16 @@
+import Link from "next/link"
+import { AiFillGithub } from "react-icons/ai"
+
 const Footer = ({ }) => {
     return (
-        <footer className='w-full h-20 bg-[#f6f3ee] mt-3 py-2 justify-center flex items-center flex-col gap-2 border-t'>
-            <p>Copyrights &copy; {new Date().getFullYear()} by Gavin dev</p>
+        <footer className='w-full h-20 bg-[#f6f3ee] mt-[100px] py-2 justify-center flex items-center flex-col gap-2 border-t'>
+            <div className="flex items-center gap-3">
+                <p>Copyrights &copy; {new Date().getFullYear()} by Gavin dev</p>
+                <div>|</div>
+                <Link href="https://github.com/vovantruong/comics-with-nextjs13" className="flex items-center hover:underline" target="_blank">
+                    <AiFillGithub /> <span className="ml-2">vovantruong</span>
+                </Link>
+            </div>
         </footer>
     )
 }
