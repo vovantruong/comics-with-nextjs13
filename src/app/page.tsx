43da81116f72df6c8e1a92786a9a6828384bf510
Vo Van Truong <vvtruong1007@gmail.com>
2024-03-16 22:44:08 +0700

    comit fix fetchData Banner in server

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,34 @@ import SectionUpdatedComics from '@/components/comon/SectionUpdatedComics';
 import RootLayout from '@/components/layout/RootLayout';
 import ListGenres from '@/components/sidebar/ListGenres';
 import RankingComics from '@/components/sidebar/RankingComics';
+import { comicsProps } from '@/types/typeProps';
 
-export default function Home() {
+const fetchDataBanner = async (page: string, limit: number) => {
+
+    try {
+        const res = await fetch(`https://comics-api.vercel.app/trending-comics${page ? `?page=${page}` : ''}`, {
+            headers: {
+                "Content-Type": "application/json",
+                'Access-Control-Allow-Origin': '*'
+            }
+        })
+        const { comics } = await res.json()
+
+        const newData = limit ? comics.filter((item: comicsProps, index: number) => index < limit) : comics;
+
+        return newData
+    } catch (error) {
+        console.log("Invalited Error - ", error);
+    }
+}
+
+export default async function Home() {
+    const dataBanner = await fetchDataBanner('', 15)
+
     return (
         <RootLayout>
             <main className={` overflow-x-hidden bg-white`}>
-                <BannerComics />
+                <BannerComics dataTrending={dataBanner} />
                 <div className='container flex items-start justify-center relative flex-wrap'>
                     <div className='xl:w-[70%] w-full xl:pr-2 md:pr-0'>
                         <div className="relative w-full">
--- src/components/comon/BannerComics.tsx
+++ src/components/comon/BannerComics.tsx
@@ -1,31 +1,19 @@
 'use client'
-import { useEffect, useRef, useState } from 'react'
+import { FC, useRef } from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { Autoplay, Pagination, FreeMode } from 'swiper/modules';
 import { comicsProps } from '@/types/typeProps'
-import { getTrending } from '@/utils/services'
 import CardComic from '@/components/customs/CardComic'
 import Skeleton from 'react-loading-skeleton';
 import clsx from 'clsx';
-import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
-import useSWR from 'swr';
+
+interface bannerProps {
+    dataTrending: comicsProps[]
+}
 
 
-const BannerComics = ({ }) => {
-
+const BannerComics: FC<bannerProps> = ({ dataTrending }) => {
     const swiperRef = useRef() as any;
-
-    const fetcherWithTrending = (url: string, headerValue: any) => fetch(url, {
-        method: 'POST',
-        body: JSON.stringify(headerValue)
-    }).then(res => res.json())
-
-    const { data: dataTrending, isLoading } = useSWR(`/api/trending`, url => fetcherWithTrending(url, { page: 1, limit: 15 }), {
-        revalidateIfStale: false,
-        revalidateOnFocus: false,
-        revalidateOnReconnect: false
-    })
-
 
     return (
         <div className='container mx-auto'>
@@ -93,7 +81,7 @@ const BannerComics = ({ }) => {
                         </div>
                     )}
                 </div>
-                {
+                {/* {
                     (!isLoading) && (
                         <div className='absolute left-0 right-0 top-[45%] -translate-y-[50%] w-full z-10 lg:opacity-0 transition-all duration-300 group-hover/pagi:opacity-100 
                 sm:block hidden opacity-100'>
@@ -113,7 +101,7 @@ const BannerComics = ({ }) => {
                             </button>
                         </div>
                     )
-                }
+                } */}
 
             </section>
         </div>
